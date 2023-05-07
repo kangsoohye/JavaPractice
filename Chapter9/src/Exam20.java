44a9d1fd649faead26f2afe60ebd6f6d0cee1f43
Kang Soo Hyeon <kangsh5721@naver.com>
2023-05-07 22:08:48 +0900

    백준 2743번

---
+++ Chapter9/src/Exam20.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+// 백준 2743번 - 단어 길이 재기
+public class Exam20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner st = new Scanner(System.in);
+		System.out.print(st.next().length());
+		
+		st.close();
+		
+	}
+
+}
