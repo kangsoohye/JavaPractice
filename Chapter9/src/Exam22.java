26d9706a1a03eb2c772822b676819e03ec746613
Kang Soo Hyeon <kangsh5721@naver.com>
2023-05-21 23:23:39 +0900

    백준 10171번

---
+++ Chapter9/src/Exam22.java
@@ -0,0 +1,13 @@
+// 백준 10171번 - 고양이
+public class Exam22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("\\   /\\");
+		System.out.print(" ) ( ')");
+		System.out.print("( /  )");
+		System.out.print(" \\(__)|");
+	}
+
+}
